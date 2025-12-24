64dcdf0bc3c265102f342f2027f1aed6a9624fa9
chsattibabu99 <sattibabuchittala1988@gmail.com>
2025-12-24 08:47:05 -0500

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+//to find area of a rectangle
+import java.util.Scanner;
+public class Rectangle {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter length value");
+		int length = s.nextInt();
+		System.out.println("Enter breadth value");
+		int breadth = s.nextInt();
+		int area = length * breadth;
+		System.out.println("Area of Rectangle = "+area);
+		int num=0; //
+		Integer value =new Integer(20);//Class
+		//value
+		System.out.println("value ",+num.);
+		value.toString();
+
+	}
+
+}
